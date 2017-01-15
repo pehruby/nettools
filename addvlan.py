3f598e450fe1072689d969abe6183be0de0ee29c
pehruby <petr.hruby@gmail.com>
2017-01-15 15:56:02 +0100

    Some changes ...

--- addvlan.py
+++ addvlan.py
@@ -4,11 +4,12 @@ import sys
 import getpass
 import re
 import getopt
+import os
 from netmiko import ConnectHandler
 
 
 def get_intlist_vlan(handler, vlan):
-    ''' Returns list of interfaces where VLAN vlan is configured
+    ''' Returns list of all (both access andf trunks) interfaces where VLAN vlan is configured
 
     '''
     str_vlan = str(vlan)
@@ -17,7 +18,7 @@ def get_intlist_vlan(handler, vlan):
     cli_out_split = cli_output.split('\n')
     for i in range(len(cli_out_split)-2):
         if re.match(r"VLAN Name\s+Status\s+Ports", cli_out_split[i]):   #find line with column names
-            print("OK")
+            # print("Matched line in "sh vlan id" with interfaces configured in Vlan", vlan)
             intstr = re.match("^"+str_vlan+r"\s+(.+?)\s+(.+?)\s+(.+)", cli_out_split[i+2]) # interfaces are on 2nd line after column names line
             if intstr:
                 return intstr.group(3).split(",")       # list of interfaces
@@ -47,8 +48,8 @@ def is_int_allowed_vlan_configured(handler, iface):
     return False
 
 def normalize_vlan_list(vlset):
-    ''' Returns list of VLAN numbers, in vlset changes VLAN ranges (i.e 2345-2358) into list of VLANs
-    covered in range
+    ''' Returns list of VLAN numbers. In vlset changes VLAN ranges (i.e 2345-2348) into list of VLANs
+    contained in range (2345,2346,2347,2348)
     '''
 
     outset = list()      # empty list
@@ -69,14 +70,17 @@ def list_trunking_vlans_enabled(handler, iface):
     cli_param = "sh int " + iface + " switchport"
     cli_output = handler.send_command(cli_param)
     intstr = re.search(r"Trunking VLANs Enabled:\s+([0-9\-,\n ]+)[A-Za-z]+", cli_output)
+    if not intstr:
+        intstr = re.search(r"Trunking VLANs Enabled:\s+(ALL)", cli_output)
     if intstr:
         tmps = intstr.group(1)
-        newtmps = tmps.replace('\n', '')        # delete newlines
-        tmps = newtmps.replace(' ', '')         # delete spaces
+        tmps = tmps.replace('ALL', '1-4096')
+        tmps = tmps.replace('\n', '')        # delete newlines
+        tmps = tmps.replace(' ', '')         # delete spaces
         vlanlist = tmps.split(",")       # list VLANs
-        print(vlanlist)
+        #print(vlanlist)
         vlanlist = normalize_vlan_list(vlanlist)
-        print("Normalized list: ", vlanlist)
+        # print("Normalized list: ", vlanlist)
         return vlanlist
     else:
         print("Expected line with interface list for iface" + iface + "was not found !!!")
@@ -136,7 +140,24 @@ def is_valid_vlan_number(vnum):
 def process_cfg_file(fname):
     ''' Read IP addresses from cfg file
     '''
-    None
+    # iplist = []
+    if os.path.isfile(fname):        # check file exists
+        try:
+            with open(fname) as data_file:
+                loadedfile = data_file.read()
+        except IOError:
+            print("Unable to read the file", fname)
+            exit(1)
+    else:
+        print("Cannot find the file", fname)
+        exit(1)
+    loadedfile = loadedfile.replace(' ', '')         # delete spaces
+    iplist = loadedfile.split("\n")
+    iplist = list(filter(None, iplist))         # delete empty lines
+    for ips in iplist:
+        if not is_ip_valid(ips):
+            print("Invalid IP address in config file:", ips)
+            exit(1)
     return iplist
 
 
@@ -151,6 +172,7 @@ def main():
     Usage: addvlan.py [OPTIONS]
     -v,     --verbose                   verbose
     -h,     --help                      display help
+    -w,     --writeconf                 write device configuration after config change (wr m)
     -i,     --ipaddr                    IP address of the switch
     -a,     --action                    test, t, process, p
     -u,     --username                  username
@@ -169,22 +191,24 @@ def main():
     username = ''
     debug = False
     pswd = ''
-
+    paction = 't'       # test mode is default
+    write_conf = False
 
     argv = sys.argv[1:]
 
     try:
-        opts, args = getopt.getopt(argv, "vhp:i:c:u:a:m:n:", ["verbose", "help", "password=", "ipaddr=", "cfgfile=", "username=", "action=", "matchedvlan=", "newvlan="])
+        opts, args = getopt.getopt(argv, "vwhp:i:c:u:a:m:n:", ["verbose", "help", "writeconf", "password=", "ipaddr=", "cfgfile=", "username=", "action=", "matchedvlan=", "newvlan="])
     except getopt.GetoptError:
         print(usage_str)
         sys.exit(2)
-    print(opts, args)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print(usage_str)
             sys.exit()
         elif opt in ("-v", "--verbose"):
             debug = True
+        elif opt in ("-w", "--write"):
+            write_conf = True
         elif opt in ("-u", "--username"):
             username = arg
         elif opt in ("-i", "--ipaddr"):
@@ -208,7 +232,7 @@ def main():
     if ip_of_switch and config_file:
         print("Both IP address and config file are specified")
         sys.exit(2)
-    if not is_ip_valid(ip_of_switch):
+    if ip_of_switch and (not is_ip_valid(ip_of_switch)):
         print("Invalid IP address")
         sys.exit(2)
     if not vlan_match:
@@ -229,22 +253,36 @@ def main():
     else:
         device_ip_list.append(ip_of_switch) # list will contain only IP of one device
 
+    print("")
+    if paction == 't':
+        print("Script is running in test mode. Devices configuration will not be affected")
+    elif paction == 'p':
+        print("Script is running in process mode. Devices Configuration WILL BE CHANGED !!!")
+
     for switch in device_ip_list:           # go throug all switches
-        print("Processing device:", switch)
+        print("\nProcessing device:", switch)
         net_connect = ConnectHandler(device_type='cisco_ios', ip=switch, username=username, password=pswd)
+        if not is_it_switch(net_connect):
+            print("-device is probably not a switch")
+            continue
         int_list = get_intlist_vlan(net_connect, vlan_match)    # get interfaces list where specific vlan configured
 
+        if int_list:    # is any interface configured in vlan_match ?
+            for interface in int_list:      # select trunk interfaces where "allowed vlans" command is configured
+                #print("Interface: " + interface)
+                tmplist = list_trunking_vlans_enabled(net_connect, interface) # trunks only
+                #print(tmplist)
+                if is_int_admin_trunk(net_connect, interface) and is_int_allowed_vlan_configured(net_connect, interface):
+                    int_trunk_list.append(interface)
 
-        for interface in int_list:
-            print("Interface: " + interface)
-            tmplist = list_trunking_vlans_enabled(net_connect, interface)
-            print(tmplist)
-            if is_int_admin_trunk(net_connect, interface) and is_int_allowed_vlan_configured(net_connect, interface):
-                int_trunk_list.append(interface)
 
-
-        print(int_list)
-        print(int_trunk_list)
+            #print(int_list)
+            #print(int_trunk_list)
+            for interface in int_trunk_list:
+                if paction == 't':    # test only
+                    print("-interface", interface, "would have been changed (test mode)")
+        else:
+            print("-no interface configured in Vlan", vlan_match)
 
         net_connect.disconnect()        # disconnect from the switch
 
